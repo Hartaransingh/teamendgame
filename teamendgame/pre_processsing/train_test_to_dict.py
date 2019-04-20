f0240930f8480e08632cb6d50806b8494caab3fd
hartaran singh <hartaranpanesar@gmail.com>
2019-04-21 00:51:01 +0530

    Add files via upload

---
+++ teamendgame/pre_processsing/train_test_to_dict.py
@@ -0,0 +1,10 @@
+"""
+This file will fetch train and test csv and then conver the data into dictionary.
+"""
+__author__="Hartaran Singh"
+
+def read_csv():
+    pass
+
+if __name__=="__main__":
+    pass
